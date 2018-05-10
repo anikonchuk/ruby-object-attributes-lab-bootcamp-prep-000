e8a55694b5ce03da65f042da03d07ac752459f19
Andrew Nikonchuk <anikonch@gmail.com>
2018-05-10 16:45:19 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,19 @@
-def Person
+class Person
 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(new_job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
 
 end
